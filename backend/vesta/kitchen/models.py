dc4443e2ae23c3d68ca7cfa6a82d661dfd61fef9
fuofwi <fullofwise@gmail.com>
2021-11-04 01:06:50 +0900

    Models(User, Profile, Preference, UserNutirion) added (issue 9)

--- backend/vesta/kitchen/models.py
+++ backend/vesta/kitchen/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+
+class Profile(models.Model):
+  user = models.OneToOneField(
+    User,
+    on_delete=models.CASCADE
+  )
+  age = models.IntegerField()
+  sex = models.CharField(max_length=1) # 'M' for Male, 'F' for Female
+  height = models.IntegerField()
+  weight = models.IntegerField()
+
+
+#### 'Preference' Model is Seperated from 'Profile'
+#### Because field type 'list' does not exist
+#### and it has two foreign key: User, Menu
+
+class Preference(models.Model): 
+  user = models.ForeignKey(
+    User,
+    on_delete=models.CASCADE
+  )
+  menu = models.ForeignKey(
+    Menu,
+    on_delete=models.CASCADE  # is it also should be delete
+  )                           # when the related menu has been deleted?
+
+#### it must be better to rename this Model
+#### EX) DateNutrition, Nutrition
+
+class UserNutrition(models.Model):
+  user= models.ForeignKey(
+    User,
+    on_delete=models.CASCADE
+  )
+  date = models.DateField()
+  calories = models.IntegerField()
+  carbs = models.IntegerField()
+  protein = models.IntegerField()
+  fat = models.IntegerField()
