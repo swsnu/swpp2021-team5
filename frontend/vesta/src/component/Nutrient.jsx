059df0f8b6114f1b32b94c67db3037fec4add8ad
Chris Hong <tippy33@naver.com>
2021-11-07 04:26:37 +0900

    add background color to nutrient

--- frontend/vesta/src/component/Nutrient.jsx
+++ frontend/vesta/src/component/Nutrient.jsx
@@ -9,35 +9,41 @@ background-color:#B3D962;
 font-size:44px;
 font-weight:600;
 color:#F28095;
-font-family:sans-serif;
+font-family:"Helvetica";
+`;
+
+const Background = styled.div`
+background-color:#F2F2F2;
 `;
 
 const Nutrient = (props) => (
-  <Container text>
-    <MenuNameHeader>{props.menu_name}</MenuNameHeader>
-    <Divider />
-    <Image
-      src="/sushi_example_image.jpeg"
-      alt="menu"
-      size="large"
-    />
-    <p>
-      Calories:
-      {props.calories}
-    </p>
-    <p>
-      Carbohydrate:
-      {props.carbs}
-    </p>
-    <p>
-      Protein:
-      {props.protein}
-    </p>
-    <p>
-      Fat:
-      {props.fat}
-    </p>
-  </Container>
+  <Background>
+    <Container text>
+      <MenuNameHeader>{props.menu_name}</MenuNameHeader>
+      <Divider />
+      <Image
+        src="/sushi_example_image.jpeg"
+        alt="menu"
+        size="large"
+      />
+      <p>
+        Calories:
+        {props.calories}
+      </p>
+      <p>
+        Carbohydrate:
+        {props.carbs}
+      </p>
+      <p>
+        Protein:
+        {props.protein}
+      </p>
+      <p>
+        Fat:
+        {props.fat}
+      </p>
+    </Container>
+  </Background>
 );
 
 export default Nutrient;
