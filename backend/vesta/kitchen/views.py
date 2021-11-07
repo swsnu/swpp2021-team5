911744327aba9052dcfab2eec2a8872c6e4f4017
fuofwi <fullofwise@gmail.com>
2021-11-08 03:06:15 +0900

    update views.py

--- backend/vesta/kitchen/views.py
+++ backend/vesta/kitchen/views.py
@@ -1,4 +1,5 @@
-import json, datetime
+import json
+import datetime
 from django.http import HttpResponse, HttpResponseNotAllowed
 from django.contrib.auth.models import User
 from django.http.response import JsonResponse
@@ -9,12 +10,15 @@ from django.views.decorators.http import require_http_methods
 from .models import Profile, UserNutrition, Preference, Menu
 
 # Create your views here.
+
+
 @ensure_csrf_cookie
 def token(request):
     if request.method == 'GET':
         return HttpResponse(status=204)
     else:
         return HttpResponseNotAllowed(['GET'])
+
 
 @require_http_methods(["POST"])
 def signup(request):
@@ -32,6 +36,7 @@ def signup(request):
     else:
         return HttpResponseNotAllowed(['POST'])
 
+
 @require_http_methods(["POST"])
 def signin(request):
     if request.method == 'POST':
@@ -47,6 +52,7 @@ def signin(request):
     else:
         return HttpResponseNotAllowed(['POST'])
 
+
 def signout(request):
     if request.method == 'GET':
         if request.user.is_authenticated:
@@ -56,6 +62,7 @@ def signout(request):
             return HttpResponse(status=401)
     else:
         return HttpResponseNotAllowed(['GET'])
+
 
 def resign(request):
     if request.method == 'DELETE':
@@ -69,6 +76,7 @@ def resign(request):
             return HttpResponse(status=401)
     else:
         return HttpResponseNotAllowed(['DELETE'])
+
 
 def profile(request):
     if request.method == 'GET':
@@ -88,7 +96,7 @@ def profile(request):
                 'sex': user.profile.sex,
                 'height': user.profile.height,
                 'weight': user.profile.weight,
-                'preference' : food_preference_list
+                'preference': food_preference_list
             }
             return JsonResponse(response_dict, status=200, safe=False)
         else:
@@ -109,7 +117,7 @@ def profile(request):
             new_height = int(req_data['height'])
             new_weight = int(req_data['weight'])
             new_food_preference_list = req_data['preference']
-            
+
             user.username = new_username
             user.profile.age = new_age
             user.profile.sex = new_sex
@@ -117,24 +125,25 @@ def profile(request):
             user.profile.weight = new_weight
             user.save()
             user.profile.save()
-            
+
             # lines below should be refactored so that pk of row could be keep
             Preference.objects.filter(user_id=request.user.id).delete()
             for food in new_food_preference_list:
                 new_menu = Menu.objects.get(name=food)
-                new_preference_item = Preference(user=request.user, menu=new_menu)
+                new_preference_item = Preference(
+                    user=request.user, menu=new_menu)
                 new_preference_item.save()
             food_preference_list_response = []
             for item in Preference.objects.filter(user_id=request.user.id):
                 food_preference_list_response.append(str(item.menu.name))
-            
+
             response_dict = {
                 'username': user.username,
                 'age': user.profile.age,
                 'sex': user.profile.sex,
                 'height': user.profile.height,
                 'weight': user.profile.weight,
-                'preference' : food_preference_list_response
+                'preference': food_preference_list_response
             }
             return JsonResponse(response_dict, status=200)
         else:
@@ -142,20 +151,25 @@ def profile(request):
     else:
         return HttpResponseNotAllowed(['GET', 'PUT'])
 
+
 def nutrition(request):
     if request.method == 'GET':
         if request.user.is_authenticated:
             req_data = json.loads(request.body.decode())
-            ## req_data['date'] comes in YYYY-MM-DD form, transform the string into datetime object
+            # req_data['date'] comes in YYYY-MM-DD form, transform the string
+            # into datetime object
             date_list = req_data['date'].split('-')
-            today = datetime.date(int(date_list[0]), int(date_list[1]), int(date_list[2]))
+            today = datetime.date(int(date_list[0]), int(
+                date_list[1]), int(date_list[2]))
             try:
-                today_nutrition = UserNutrition.objects.get(user_id=request.user.id, date=today)
+                today_nutrition = UserNutrition.objects.get(
+                    user_id=request.user.id, date=today)
             except UserNutrition.DoesNotExist:      # User.DoesNotExist?
                 return HttpResponse(status=404)
             response_dict = {
                 'user_id': today_nutrition.user_id,
-                'date': today_nutrition.date.strftime('%Y-%m-%d'), # is it okay?
+                # is it okay?
+                'date': today_nutrition.date.strftime('%Y-%m-%d'),
                 'calories': today_nutrition.calories,
                 'carbs': today_nutrition.carbs,
                 'protein': today_nutrition.protein,
@@ -168,13 +182,20 @@ def nutrition(request):
         if request.user.is_authenticated:
             req_data = json.loads(request.body.decode())
             date_list = req_data['date'].split('-')
-            today = datetime.date(int(date_list[0]), int(date_list[1]), int(date_list[2]))
+            today = datetime.date(int(date_list[0]), int(
+                date_list[1]), int(date_list[2]))
             calories = int(req_data['calories'])
             carbs = int(req_data['carbs'])
             protein = int(req_data['protein'])
             fat = int(req_data['fat'])
 
-            new_record = UserNutrition(user=request.user, date=today, calories=calories, carbs=carbs, protein=protein, fat=fat)
+            new_record = UserNutrition(
+                user=request.user,
+                date=today,
+                calories=calories,
+                carbs=carbs,
+                protein=protein,
+                fat=fat)
             new_record.save()
 
             response_dict = {
@@ -193,9 +214,11 @@ def nutrition(request):
         if request.user.is_authenticated:
             req_data = json.loads(request.body.decode())
             date_list = req_data['date'].split('-')
-            today = datetime.date(int(date_list[0]), int(date_list[1]), int(date_list[2]))
+            today = datetime.date(int(date_list[0]), int(
+                date_list[1]), int(date_list[2]))
             try:
-                today_nutrition = UserNutrition.objects.get(user_id=request.user.id, date=today)
+                today_nutrition = UserNutrition.objects.get(
+                    user_id=request.user.id, date=today)
             except UserNutrition.DoesNotExist:    # User.DoesNotExist?
                 return HttpResponse(status=404)
 
@@ -214,7 +237,7 @@ def nutrition(request):
             response_dict = {
                 'id': today_nutrition.id,
                 'user_id': today_nutrition.user_id,
-                'date': today_nutrition.date.strftime('%Y-%m-%d'),  # is it right?
+                'date': today_nutrition.date.strftime('%Y-%m-%d'),
                 'calories': today_nutrition.calories,
                 'carbs': today_nutrition.carbs,
                 'protein': today_nutrition.protein,
