ace9a98e7cf9d6e60c479f98ee530dee97ffbc2a
Chris Hong <tippy33@naver.com>
2021-11-07 23:36:22 +0900

    apply styled component

--- frontend/vesta/src/component/Nutrient.jsx
+++ frontend/vesta/src/component/Nutrient.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Image, Container, Divider,
+  Image, Container, Divider, Grid,
 } from 'semantic-ui-react';
 import styled from 'styled-components';
 
@@ -10,40 +10,48 @@ font-size:44px;
 font-weight:600;
 color:#F28095;
 font-family:"Helvetica";
+border-radius: 10px;
 `;
 
-const Background = styled.div`
-background-color:#F2F2F2;
+const TextArea = styled.div`
+background-color:#F2CE1B;
+border-radius: 10%;
 `;
 
 const Nutrient = (props) => (
-  <Background>
-    <Container text>
-      <MenuNameHeader>{props.menu_name}</MenuNameHeader>
-      <Divider />
-      <Image
-        src="/sushi_example_image.jpeg"
-        alt="menu"
-        size="large"
-      />
-      <p>
-        Calories:
-        {props.calories}
-      </p>
-      <p>
-        Carbohydrate:
-        {props.carbs}
-      </p>
-      <p>
-        Protein:
-        {props.protein}
-      </p>
-      <p>
-        Fat:
-        {props.fat}
-      </p>
-    </Container>
-  </Background>
+  <Container text>
+    <MenuNameHeader>{props.menu_name}</MenuNameHeader>
+    <Divider />
+    <Grid>
+      <Grid.Column width={12}>
+        <Image
+          src="/sushi_example_image.jpeg"
+          alt="menu"
+          size="large"
+        />
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <TextArea>
+          <p>
+            Calories:
+            {props.calories}
+          </p>
+          <p>
+            Carbohydrate:
+            {props.carbs}
+          </p>
+          <p>
+            Protein:
+            {props.protein}
+          </p>
+          <p>
+            Fat:
+            {props.fat}
+          </p>
+        </TextArea>
+      </Grid.Column>
+    </Grid>
+  </Container>
 );
 
 export default Nutrient;
