5b64e38397737b66a4d7d78c29eff9efb99e204f
fuofwi <fullofwise@gmail.com>
2021-11-08 01:56:21 +0900

    finished revising backend setting except for testing, issue9

--- backend/vesta/kitchen/urls.py
+++ backend/vesta/kitchen/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('user/signout/', views.signout, name='signout'),
     path('user/resign/', views.resign, name='resign'),
     path('user/profile/', views.profile, name='profile'),
-    path('nutrition/', views.nutrition, name='nutrition')
+    path('nutrition/', views.nutrition, name='nutrition'),
+    path('token/', views.token, name='token')
 ]
