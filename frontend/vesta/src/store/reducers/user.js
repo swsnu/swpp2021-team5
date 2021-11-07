2eaff4fdcb115629a2efc9bc186b4e468aa7a6c4
Manhyung Han <Manhyung@Manhyungs-MacBook-Pro-3.local>
2021-11-07 20:34:24 +0900

    travis updated

---
+++ frontend/vesta/src/store/reducers/user.js
@@ -0,0 +1,14 @@
+const initialState = {
+  user: null,
+  currentUser: null,
+  userNutrition: null,
+};
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      break;
+  }
+  return state;
+};
+
+export default userReducer;
