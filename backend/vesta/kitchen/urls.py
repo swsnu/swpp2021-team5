58fcb9e6ac1bac1d5924e2c27ea6f5d0e9c39fbf
Manhyung Han <Manhyung@Manhyungs-MacBook-Pro-3.local>
2021-11-08 10:38:52 +0900

    resolved pylint and included pillow

--- backend/vesta/kitchen/urls.py
+++ backend/vesta/kitchen/urls.py
@@ -5,11 +5,11 @@ from . import views
 
 urlpatterns = [
     path('record/', views.record, name='record'),
-    path('record/<int:record_id>/', views.record_id, name='recordID'),
+    path('record/<int:record_id>/', views.record_ID, name='recordID'),
     path('record/user/<int:user_id>/', views.record_user_id, name='recordUserID'),
-    path('record/<int:record_id>/review/', views.review, name='review'),
-    path('recipe/<str:menu_name>/', views.recipe_menu_name, name='recipeMenuName'),
+    path('record/<int:review_record_id>/review/', views.review, name='review'),
+    path('recipe/<str:menu_name_recipe>/', views.recipe_menu_name, name='recipeMenuName'),
     path('menu/', views.menu, name='menu'),
-    path('menu/<str:menu_name>/', views.menu_name, name='menuName'),
+    path('menu/<str:menuname>/', views.menu_name, name='menuName'),
     path('token/', views.token, name='token'),
 ]
