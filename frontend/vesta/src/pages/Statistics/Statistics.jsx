c603f17343504d105e41be4de5252c34626936b6
fuofwi <fullofwise@gmail.com>
2021-11-09 00:18:31 +0900

    add statistics files

---
+++ frontend/vesta/src/pages/Statistics/Statistics.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import {
+  Header, Table, Input, Button,
+} from 'semantic-ui-react';
+
+import * as actionCreators from '../../store/actions/index';
+
+
+class Statistics extends Component {
+
+    render() {
+        return (
+            <div className="Statistics">
+
+                
+            </div>
+        )
+    }
+}
