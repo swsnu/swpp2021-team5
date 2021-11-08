ea7c4b790c7b044577f7c21099696be1680118df
fuofwi <fullofwise@gmail.com>
2021-11-08 16:36:26 +0900

    fixed errors detected by pylint

--- backend/vesta/kitchen/tests.py
+++ backend/vesta/kitchen/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+import json
+from django.test import TestCase, Client
 
 # Create your tests here.
+
+class KitchenTestCase(TestCase):
+    def test_csrf(self):
+        # By default, csrf checks are disabled in test client
+        # To test csrf protection we enforce csrf checks here
+        client = Client(enforce_csrf_checks=True)
+        response = client.post('/api/user/signup/', json.dumps({'username': 'chris', 'password': 'chris'}),
+                               content_type='application/json')
+        self.assertEqual(response.status_code, 403)  # Request without csrf token returns 403 response
+
+        response = client.get('/api/token/')
+        csrftoken = response.cookies['csrftoken'].value  # Get csrf token from cookie
+
+        client2 = Client()
+        response = client2.post('/api/token/')
+        self.assertEqual(response.status_code, 405)
+
+        response = client.get('/api/user/signup/')
+        self.assertEqual(response.status_code, 405)
+
+        response = client.post('/api/user/signup/', json.dumps({'username': 'chris', 'password': 'chris'}),
+                               content_type='application/json', HTTP_X_CSRFTOKEN=csrftoken)
+        self.assertEqual(response.status_code, 201)  # Pass csrf protection
+        
