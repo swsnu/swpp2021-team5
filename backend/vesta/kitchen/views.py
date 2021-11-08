f5017dce91a347723041cfa96efcee287abc1c86
Manhyung Han <Manhyung@Manhyungs-MacBook-Pro-3.local>
2021-11-08 09:53:26 +0900

    resolved pylint

--- backend/vesta/kitchen/views.py
+++ backend/vesta/kitchen/views.py
@@ -1,24 +1,24 @@
+import json
+import datetime
 from django.http import HttpResponse, HttpResponseNotAllowed
 from django.http.response import JsonResponse
-from django.views.decorators.csrf import csrf_exempt, csrf_protect, ensure_csrf_cookie
+from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.http import require_GET, require_http_methods
-import json
 from .models import Menu, Recipe, Record
-import datetime
 
 # Create your views here.
-@require_http_methods(['GET', 'POST'])
+@require_http_methods(["GET", "POST"])
 def record(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         ## If user is not signed in, respond with 401
         if not request.user.is_authenticated:
             return HttpResponse(status = 401)
 
         ## return all records
-        all_record_list = [record for record in Record.objects.all().values()]
+        all_record_list = list(Record.objects.all().values())
         return JsonResponse(all_record_list, safe=False)
 
-    elif request.method == 'POST':
+    if request.method == "POST":
         ## If user is not signed in, respond with 401
         if not request.user.is_authenticated:
             return HttpResponse(status = 401)
@@ -27,26 +27,35 @@ def record(request):
         req_data = json.loads(request.body.decode())
         menu_id = req_data['menu_id']
         recipe_id = req_data['recipe_id']
-        review = req_data['review']
-        liked = True if req_data['liked'] == "True" else False
+        review_text = req_data['review']
+        liked = req_data['liked'] == "True"
         ## req_data['date'] comes in YYYY-MM-DD form, transform the string into datetime object
         date_list = req_data['date'].split('-')
         date = datetime.date(int(date_list[0]), int(date_list[1]), int(date_list[2]))
 
-        record = Record(user = request.user, menu = Menu.objects.get(id = menu_id), 
-                        recipe = Recipe.objects.get(id = recipe_id), review = review, liked = liked, date = date)
-        record.save()
+        newRecord = Record(user = request.user,
+                        menu = Menu.objects.get(id = menu_id),
+                        recipe = Recipe.objects.get(id = recipe_id),
+                        review = review_text,
+                        liked = liked,
+                        date = date)
+        newRecord.save()
 
         ## respond with created record detail
-        response_dict = {'id' : record.id, 'user' : record.user, 'menu' : record.menu, 'recipe' : record.recipe,
-                        'review' : record.review, 'liked' : record.liked, 'date' : record.date}
-        return HttpResponse(json.dumps(response_dict), status = 201)
+        response_dict = {'id' : newRecord.id,
+                        'user' : newRecord.user,
+                        'menu' : newRecord.menu,
+                        'recipe' : newRecord.recipe,
+                        'review' : newRecord.review,
+                        'liked' : newRecord.liked,
+                        'date' : newRecord.date}
+        return JsonResponse(response_dict)
 
     else:
-        return HttpResponseNotAllowed(['GET', 'POST'])
+        return HttpResponseNotAllowed(["GET", "POST"])
 
 @require_GET
-def recordID(request, record_id):
+def record_id(request, record_id):
     if request.method == 'GET':
         ## If user is not signed in, respond with 401
         if not request.user.is_authenticated:
@@ -57,17 +66,18 @@ def recordID(request, record_id):
             return HttpResponse(status = 404)
 
         ## return record of record_id
-        matchingRecord = Record.objects.get(id = record_id)
-        response_dict = {'id' : record_id, 'user_id' : matchingRecord.user.id, 'menu_id' : matchingRecord.menu.id, 
-                        'recipe_id' : matchingRecord.recipe.id, 'review' : matchingRecord.review, 
-                        'liked' : matchingRecord.liked, 'date' : matchingRecord.date}
+        matching_record = Record.objects.get(id = record_id)
+        response_dict = {'id' : record_id,
+                        'user_id' : matching_record.user.id,
+                        'menu_id' : matching_record.menu.id,
+                        'recipe_id' : matching_record.recipe.id,
+                        'review' : matching_record.review,
+                        'liked' : matching_record.liked,
+                        'date' : matching_record.date}
         return JsonResponse(response_dict)
 
-    else:
-        return HttpResponseNotAllowed(['GET'])
-
 @require_GET
-def recordUserID(request, user_id):
+def record_user_id(request, user_id):
     if request.method == 'GET':
         ## If user is not signed in, respond with 401
         if not request.user.is_authenticated:
@@ -80,16 +90,15 @@ def recordUserID(request, user_id):
         if len(all_record_list) == 0:
             return HttpResponse(status = 404)
         ## else, return records
-        else:
-            return JsonResponse(all_record_list, safe = False)
+        return JsonResponse(all_record_list, safe = False)
 
     else:
         return HttpResponseNotAllowed(['GET'])
 
 
-@require_http_methods(['GET', 'POST', 'PUT', 'DELETE'])
+
 def review(request, record_id):
-    if request.method == 'GET':
+    if request.method == "GET":
         ## If user is not signed in, respond with 401
         if not request.user.is_authenticated:
             return HttpResponse(status = 401)
@@ -98,86 +107,75 @@ def review(request, record_id):
         if not Record.objects.filter(id = record_id).exists():
             return HttpResponse(status = 404)
 
-        matchingReview = Record.objects.get(id = record_id).review
-        return JsonResponse({'review' : matchingReview})
+        matching_review = Record.objects.get(id = record_id).review
+        return JsonResponse({'review' : matching_review})
 
-    elif request.method == 'POST':
-        ## If user is not signed in, respond with 401
-        if not request.user.is_authenticated:
+    
+    ## If user is not signed in, respond with 401
+    if not request.user.is_authenticated:
             return HttpResponse(status = 401)
 
-        ## If record of record_id does not exist, respond with 404
-        if not Record.objects.filter(id = record_id).exists():
-            return HttpResponse(status = 404)
+    ## If record of record_id does not exist, respond with 404
+    if not Record.objects.filter(id = record_id).exists():
+        return HttpResponse(status = 404)
 
-        ## If post request is not from the user of the record, respond with 403
-        if Record.objects.get(id = record_id).user_id.id != request.user.id:
-            return HttpResponse(status = 403)
+    ## If post request is not from the user of the record, respond with 403
+    if Record.objects.get(id = record_id).user_id.id != request.user.id:
+        return HttpResponse(status = 403)
 
+    if request.method == "POST":
         ## create review in selected record
-        review = json.loads(request.body.decode())['review']
-        recordToAddReview = Record.objects.get(id = record_id)
-        recordToAddReview.review = review
-        recordToAddReview.save()
+        review_post = json.loads(request.body.decode())['review']
+        record_to_add_review = Record.objects.get(id = record_id)
+        record_to_add_review.review = review_post
+        record_to_add_review.save()
 
         ## respond with the edited record
-        response_dict = {'id' : record_id, 'user_id' : recordToAddReview.user.id, 'menu_id' : recordToAddReview.menu.id, 
-                        'recipe_id' : recordToAddReview.recipe.id, 'review' : recordToAddReview.review, 
-                        'liked' : recordToAddReview.liked, 'date' : recordToAddReview.date}
-        return HttpResponse(json.dumps(response_dict), status = 200)
+        response_dict = {'id' : record_id,
+                        'user_id' : record_to_add_review.user.id,
+                        'menu_id' : record_to_add_review.menu.id,
+                        'recipe_id' : record_to_add_review.recipe.id,
+                        'review' : record_to_add_review.review,
+                        'liked' : record_to_add_review.liked,
+                        'date' : record_to_add_review.date}
+        return JsonResponse(response_dict)
 
-    elif request.method == 'PUT':
-        ## If user is not signed in, respond with 401
-        if not request.user.is_authenticated:
-            return HttpResponse(status = 401)
-
-        ## If record of record_id does not exist, respond with 404
-        if not Record.objects.filter(id = record_id).exists():
-            return HttpResponse(status = 404)
-
-        ## If put request is not from the user of the record, respond with 403
-        if Record.objects.get(id = record_id).user_id.id != request.user.id:
-            return HttpResponse(status = 403)
-
+    if request.method == "PUT":
         ## edit review in selected record
-        newReview = json.loads(request.body.decode())['review']
-        recordToEditReview = Record.objects.get(id = record_id)
-        recordToEditReview.review = newReview
-        recordToEditReview.save()
+        new_review = json.loads(request.body.decode())['review']
+        record_to_edit_review = Record.objects.get(id = record_id)
+        record_to_edit_review.review = new_review
+        record_to_edit_review.save()
 
         ## respond with the edited record
-        response_dict = {'id' : record_id, 'user_id' : recordToEditReview.user.id, 'menu_id' : recordToEditReview.menu.id, 
-                        'recipe_id' : recordToEditReview.recipe.id, 'review' : recordToEditReview.review, 
-                        'liked' : recordToEditReview.liked, 'date' : recordToEditReview.date}
-        return HttpResponse(json.dumps(response_dict), status = 200)
+        response_dict = {'id' : record_id,
+                        'user_id' : record_to_edit_review.user.id,
+                        'menu_id' : record_to_edit_review.menu.id,
+                        'recipe_id' : record_to_edit_review.recipe.id,
+                        'review' : record_to_edit_review.review,
+                        'liked' : record_to_edit_review.liked,
+                        'date' : record_to_edit_review.date}
+        return JsonResponse(response_dict)
 
-    elif request.method == 'DELETE':
-        ## If user is not signed in, respond with 401
-        if not request.user.is_authenticated:
-            return HttpResponse(status = 401)
-
-        ## If record of record_id does not exist, respond with 404
-        if not Record.objects.filter(id = record_id).exists():
-            return HttpResponse(status = 404)
-
-        ## If delete request is not from the user of the record, respond with 403
-        if Record.objects.get(id = record_id).user_id.id != request.user.id:
-            return HttpResponse(status = 403)
-
+    if request.method == "DELETE":
         ## delete review of selected record
-        recordToDeleteReview = Record.objects.get(id = record_id)
-        recordToDeleteReview.review = ""
-        recordToDeleteReview.save()
+        record_to_delete_review = Record.objects.get(id = record_id)
+        record_to_delete_review.review = ""
+        record_to_delete_review.save()
 
         ## respond with the edited record
-        response_dict = {'id' : record_id, 'user_id' : recordToDeleteReview.user.id, 'menu_id' : recordToDeleteReview.menu.id, 
-                        'recipe_id' : recordToDeleteReview.recipe.id, 'review' : recordToDeleteReview.review, 
-                        'liked' : recordToDeleteReview.liked, 'date' : recordToDeleteReview.date}
-        return HttpResponse(json.dumps(response_dict), status = 200)
+        response_dict = {'id' : record_id,
+                        'user_id' : record_to_delete_review.user.id,
+                        'menu_id' : record_to_delete_review.menu.id,
+                        'recipe_id' : record_to_delete_review.recipe.id,
+                        'review' : record_to_delete_review.review,
+                        'liked' : record_to_delete_review.liked,
+                        'date' : record_to_delete_review.date}
+        return JsonResponse(response_dict)
 
 
 @require_GET
-def recipeMenuName(request, menu_name):
+def recipe_menu_name(request, menu_name):
     if request.method == 'GET':
         ## If user is not signed in, respond with 401
         if not request.user.is_authenticated:
@@ -188,18 +186,16 @@ def recipeMenuName(request, menu_name):
             return HttpResponse(status = 404)
 
         ## find the menu id and the recipe object corresponding to the menu_name
-        matchingMenuID = Menu.objects.get(name = menu_name).id
-        matchingRecipe = [recipe for recipe in Recipe.objects.all().values() if recipe["menu_id"] == matchingMenuID]
+        matching_menu_id = Menu.objects.get(name = menu_name).id
+        matching_recipe = [recipe for recipe in Recipe.objects.all().values() if recipe["menu_id"] == matching_menu_id]
 
         ## if there are no recipes corresponding to the menu id, respond with 404
         ## else, return the correct recipe
-        if len(matchingRecipe) == 0:
+        if len(matching_recipe) == 0:
             return HttpResponse(status = 404)
-        else:
-            return JsonResponse({'recipe' : matchingRecipe[0]["recipe"]})
+        return JsonResponse({'recipe' : matching_recipe[0]["recipe"]})
 
-    else:
-        return HttpResponseNotAllowed(['GET'])
+    return HttpResponseNotAllowed(['GET'])
 
 
 @require_GET
@@ -210,7 +206,7 @@ def menu(request):
             return HttpResponse(status = 401)
 
         ## return all menus
-        all_menu_list = [menu for menu in Menu.objects.all().values()]
+        all_menu_list = list(Menu.objects.all().values())
         return JsonResponse(all_menu_list, safe=False)
 
     else:
@@ -219,7 +215,7 @@ def menu(request):
 
 
 @require_GET
-def menuName(request, menu_name):
+def menu_name(request, menu_name):
     if request.method == 'GET':
         ## If user is not signed in, respond with 401
         if not request.user.is_authenticated:
@@ -230,13 +226,12 @@ def menuName(request, menu_name):
             return HttpResponse(status = 404)
 
         ## return corresponding menu
-        matchingMenu = Menu.objects.get(name = menu_name)
-        response_dict = {'id' : matchingMenu.id, 'name' : menu_name, 'calories' : matchingMenu.calories, 
-                        'protein' : matchingMenu.protein, 'fat' : matchingMenu.fat, 'image' : matchingMenu.image.url}
+        matching_menu = Menu.objects.get(name = menu_name)
+        response_dict = {'id' : matching_menu.id, 'name' : menu_name, 'calories' : matching_menu.calories,
+                        'protein' : matching_menu.protein, 'fat' : matching_menu.fat, 'image' : matching_menu.image.url}
         return JsonResponse(response_dict)
 
-    else:
-        return HttpResponseNotAllowed(['GET'])
+    return HttpResponseNotAllowed(['GET'])
 
 
 @ensure_csrf_cookie
@@ -244,5 +239,4 @@ def menuName(request, menu_name):
 def token(request):
     if request.method == 'GET':
         return HttpResponse(status=204)
-    else:
-        return HttpResponseNotAllowed(['GET'])
+    return HttpResponseNotAllowed(['GET'])
