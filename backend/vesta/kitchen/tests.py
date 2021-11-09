44c55ac9e82f3e7d4effc73890517670455664df
Manhyung Han <Manhyung@Manhyungs-MacBook-Pro-3.local>
2021-11-09 13:23:19 +0900

    testing done

--- backend/vesta/kitchen/tests.py
+++ backend/vesta/kitchen/tests.py
@@ -15,12 +15,6 @@ class KitchenTestClass(TestCase):
         client2 = Client()
         client2.login(username='testuser', password='testpassword')
 
-        ## GET TEST
-        ## if client is not signed in, response to GET request should be 401
-        response = client1.get('/api/record/')
-        self.assertEqual(response.status_code, 401)
-        
-        ## if client is signed in, response to GET request should be 200 with correct content
         menu1 = Menu.objects.create(name='testmenu', calories=1, carbs=1, protein=1,
                                     fat=1, image='./images/brownie.jpeg')
         recipe1 = Recipe.objects.create(menu=menu1, recipe='recipe1')
@@ -28,6 +22,13 @@ class KitchenTestClass(TestCase):
                             review='review1', liked=True,
                             date=datetime.date(2021,11,1),
                             image='./record_images/brownie.jpeg')
+
+        ## GET TEST
+        ## if client is not signed in, response to GET request should be 401
+        response = client1.get('/api/record/')
+        self.assertEqual(response.status_code, 401)
+        
+        ## if client is signed in, response to GET request should be 200 with correct content
         response = client2.get('/api/record/')
         self.assertEqual(response.status_code, 200)
         self.assertIn('[{"id": 1, "user_id": 1, "menu_id": 1, "recipe_id": 1, "review": "review1", "liked": true, "date": "2021-11-01", "image": "./record_images/brownie.jpeg"}]',response.content.decode())
@@ -39,9 +40,15 @@ class KitchenTestClass(TestCase):
         self.assertEqual(response.status_code, 401)
 
         ## if client is signed in, response to POST request should be 200 with correct content
-        #response = client2.post('/api/record/', json.dumps({'menu_id': '1', 'recipe_id': '1', 'review': 'review1', 'liked': 'True', 'date': '2011-11-11', 'image': './record_images/brownie.jpeg'}),
-         #                       content_type='application/json')
-        #self.assertEqual(response.status_code, 200)
+        response = client2.post('/api/record/', {'menu_id': 1, 'recipe_id': 1, 'review': 'review2', 'liked': 'False', 'date': '2021-11-11', 'image': './record_images/brownie.jpeg'}, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual('{"id": 2, "user_id": 1, "menu_id": 1, "recipe_id": 1, "review": "review2", "liked": false, "date": "2021-11-11", "image": "/record_images/brownie.jpeg"}', response.content.decode())
+
+
+        ## NOT GET, POST TEST
+        response = client2.put('/api/record/')
+        self.assertEqual(response.status_code, 405)
+        
 
     ## test api/record/<int:record_id>/
     def test_record_id(self):
@@ -75,6 +82,47 @@ class KitchenTestClass(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual('{"id": 1, "user_id": 1, "menu_id": 1, "recipe_id": 1, "review": "review1", "liked": true, "date": "2021-11-01", "image": "/record_images/brownie.jpeg"}', response.content.decode())
 
+
+        ## NOT GET TEST
+        response = client1.post('/api/record/1/')
+        self.assertEqual(response.status_code, 405)
+
+
+    ## test /api/record/user/<int:user_id>/
+    def test_record_user_id(self):
+        user = User.objects.create(username='testuser')
+        user.set_password('testpassword')
+        user.save()
+
+        client1 = Client()
+        client2 = Client()
+        client2.login(username='testuser', password='testpassword')
+
+        menu1 = Menu.objects.create(name='testmenu', calories=1, carbs=1, protein=1,
+                                    fat=1, image='./images/brownie.jpeg')
+        recipe1 = Recipe.objects.create(menu=menu1, recipe='recipe1')
+        Record.objects.create(user=user, menu=menu1, recipe=recipe1,
+                            review='review1', liked=True,
+                            date=datetime.date(2021,11,1),
+                            image='./record_images/brownie.jpeg')
+
+        ## GET TEST
+        ## if client is not signed in, response to GET request should be 401
+        response = client1.get('/api/record/user/1/')
+        self.assertEqual(response.status_code, 401)
+
+        ## if record whose user is user_id does not exist, response should be 404
+        response = client2.get('/api/record/user/2/')
+        self.assertEqual(response.status_code, 404)
+
+        ## correct response test
+        response = client2.get('/api/record/user/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual('[{"id": 1, "user_id": 1, "menu_id": 1, "recipe_id": 1, "review": "review1", "liked": true, "date": "2021-11-01", "image": "./record_images/brownie.jpeg"}]', response.content.decode())
+
+
+
+    ## test /api/record/<int:review_record_id>/review/
     def test_review(self):
         user = User.objects.create(username='testuser')
         user.set_password('testpassword')
@@ -128,3 +176,111 @@ class KitchenTestClass(TestCase):
         response = client2.delete('/api/record/1/review/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual('{"id": 1, "user_id": 1, "menu_id": 1, "recipe_id": 1, "review": "", "liked": true, "date": "2021-11-01"}', response.content.decode())
+
+
+    ## test api/recipe/<str:menu_name_recipe>/
+    def test_recipe_menu_name(self):
+        user = User.objects.create(username='testuser')
+        user.set_password('testpassword')
+        user.save()
+
+        client1 = Client()
+        client2 = Client()
+        client2.login(username='testuser', password='testpassword')
+
+        menu1 = Menu.objects.create(name='testmenu', calories=1, carbs=1, protein=1,
+                                    fat=1, image='./images/brownie.jpeg')
+        Menu.objects.create(name='testmenu2', calories=2, carbs=2, protein=2,
+                                    fat=2, image='./images/brownie.jpeg')
+        Recipe.objects.create(menu=menu1, recipe='recipe1')
+
+        ## GET TEST
+        ## if client is not signed in, response to GET request should be 401
+        response = client1.get('/api/recipe/testmenu/')
+        self.assertEqual(response.status_code, 401)
+
+        ## if menu with name menu_name_recipe does not exist, response should be 404
+        response = client2.get('/api/recipe/notamenu/')
+        self.assertEqual(response.status_code, 404)
+
+        ## if menu with name menu_name_recipe exists but the corresponding recipe does not exist, response should be 404
+        response = client2.get('/api/recipe/testmenu2/')
+        self.assertEqual(response.status_code, 404)
+
+        ## correct response test
+        response = client2.get('/api/recipe/testmenu/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual('{"recipe": "recipe1"}', response.content.decode())
+
+    ## test api/menu/
+    def test_menu(self):
+        user = User.objects.create(username='testuser')
+        user.set_password('testpassword')
+        user.save()
+
+        client1 = Client()
+        client2 = Client()
+        client2.login(username='testuser', password='testpassword')
+
+        Menu.objects.create(name='testmenu', calories=1, carbs=1, protein=1,
+                            fat=1, image='./images/brownie.jpeg')
+
+        
+        ## GET TEST
+        ## if client is not signed in, response to GET request should be 401
+        response = client1.get('/api/menu/')
+        self.assertEqual(response.status_code, 401)
+
+        ## correct response test
+        response = client2.get('/api/menu/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual('[{"id": 1, "name": "testmenu", "calories": 1, "carbs": 1, "protein": 1, "fat": 1, "image": "./images/brownie.jpeg"}]', response.content.decode())
+
+
+    ## test api/menu/<str:menuname>/
+    def test_menu_name(self):
+        user = User.objects.create(username='testuser')
+        user.set_password('testpassword')
+        user.save()
+
+        client1 = Client()
+        client2 = Client()
+        client2.login(username='testuser', password='testpassword')
+
+        Menu.objects.create(name='testmenu', calories=1, carbs=1, protein=1,
+                            fat=1, image='./images/brownie.jpeg')
+
+        ## GET TEST
+        ## if client is not signed in, response to GET request should be 401
+        response = client1.get('/api/menu/testmenu/')
+        self.assertEqual(response.status_code, 401)
+
+        ## if menu with name menuname does not exist, response should be 404
+        response = client2.get('/api/menu/notamenu/')
+        self.assertEqual(response.status_code, 404)
+
+        ## correct response test
+        response = client2.get('/api/menu/testmenu/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual('{"id": 1, "name": "testmenu", "calories": 1, "carbs": 1, "protein": 1, "fat": 1, "image": "/images/brownie.jpeg"}', response.content.decode())
+
+
+    ## test api/token/
+    def test_csrf(self):
+        client = Client(enforce_csrf_checks=True)
+
+        response = client.post('/api/record/1/review/', {},
+                               content_type='application/json')
+        self.assertEqual(response.status_code, 403)  # Request without csrf token returns 403 response
+        
+        response = client.get('/api/token/')
+        csrftoken = response.cookies['csrftoken'].value  # Get csrf token from cookie
+        
+        response = client.post('/api/record/1/review/', {},
+                               content_type='application/json', HTTP_X_CSRFTOKEN=csrftoken)
+        self.assertEqual(response.status_code, 401)  # Pass csrf protection
+
+        client = Client()
+        response = client.post('/api/token/')
+        self.assertEqual(response.status_code, 405)
+        
