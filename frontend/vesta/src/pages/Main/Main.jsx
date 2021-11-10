eaf970e1bfda988c3ce04a1864b976bb97bd8beb
Manhyung Han <Manhyung@Manhyungs-MacBook-Pro-3.local>
2021-11-10 20:03:35 +0900

    pages implemented

--- frontend/vesta/src/pages/Main/Main.jsx
+++ frontend/vesta/src/pages/Main/Main.jsx
@@ -4,6 +4,7 @@ import {
   Grid, Image, Button, Container,
 } from 'semantic-ui-react';
 import { withRouter } from 'react-router-dom';
+import PastNutritionInfo from '../../component/PastNutritionInfo/PastNutritionInfo';
 
 const NextMealHeader = styled.div`
 background-color:#F28095;
@@ -12,16 +13,23 @@ font-family:'verveine';
 font-size:20px;
 `;
 
-const TextArea = styled.div`
-background-color:#F2BB16;
+const MealRecordArea = styled.div`
+background-color:#B3D962;
 border-radius: 20px;
 font-family:'verveine';
-font-size:25px;
+font-size:30px;
 `;
 
-const ButtonArea = styled.div`
-background-color:#B3D962;
-border-radius: 1px;
+const StatsHeader = styled.div`
+background-color:#F2BB16;
+border-radius: 10px;
+font-family:'verveine';
+font-size:20px;
+`;
+
+const StatsBody = styled.div`
+background-color:#BDD9A0;
+border-radius: 20px;
 font-family:'verveine';
 font-size:30px;
 `;
@@ -46,39 +54,71 @@ class Main extends Component {
     this.props.history.push('/record');
   }
 
+  onClickedMenuRecButton = () => {
+    this.props.history.push('/recommendation');
+  }
+
+  onClickedPastMealRecordButton = () => {
+    this.props.history.push('/history');
+  }
+
   render() {
     return (
       <div>
         <Container>
-          <NextMealHeader>
-            <h1>Next Meal</h1>
-          </NextMealHeader>
           <Grid>
-            <Grid.Column width={9}>
-              <TextArea>
+            <Grid.Column width={8}>
+              <NextMealHeader>
+                <h1>Next Meal</h1>
+              </NextMealHeader>
+              <MealRecordArea>
                 <p>{this.state.menu}</p>
-              </TextArea>
-              <Image
-                src="/sushi_example_image.jpeg"
-                alt="menu"
-                size="centered large"
-              />
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <ButtonArea>
+                <Image
+                  src="/sushi_example_image.jpeg"
+                  alt="menu"
+                  size="centered large"
+                />
+                <Button id="menu-recommendation-button" onClick={this.onClickedMenuRecButton}>
+                  More Recommendations
+                </Button>
+                <p>
+                  Did you skip a meal? Press Skipped Meal.
+                </p>
+                <Button id="skip-meal-button" onClick={this.onClickedSkipButton}>
+                  Skipped Meal
+                </Button>
                 <p>
                   If you want to record a meal,
                   please tell us if you followed our recommendation.
                 </p>
-                <Button onClick={this.onClickedFollowedRecButton}>Followed Recommendation</Button>
-                <Button onClick={this.onClickedNotFollowedButton}>
+                <Button id="followed-recommendation-button" onClick={this.onClickedFollowedRecButton}>
+                  Followed Recommendation
+                </Button>
+                <Button id="not-followed-recommendation-button" onClick={this.onClickedNotFollowedButton}>
                   Didnt Follow Recommendation
                 </Button>
                 <p>
                   Or if you just want to record a snack, press Record Snack.
                 </p>
-                <Button onClick={this.onClickedRecordSnackButton}>Record Snack</Button>
-              </ButtonArea>
+                <Button id="record-snack-button" onClick={this.onClickedRecordSnackButton}>
+                  Record Snack
+                </Button>
+                <p />
+                <Button id="past-meal-record-button" onClick={this.onClickedPastMealRecordButton}>
+                  My Records
+                </Button>
+              </MealRecordArea>
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <StatsHeader>
+                <h1>My Stats</h1>
+              </StatsHeader>
+              <StatsBody>
+                <PastNutritionInfo />
+                <Button id="nutrition-analysis-button" onClick={this.onClickedNutritionAnalysisButton}>
+                  More Stats
+                </Button>
+              </StatsBody>
             </Grid.Column>
           </Grid>
         </Container>
