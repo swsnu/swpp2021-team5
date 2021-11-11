4158e80a960886f2644247c6483276450347b49e
fuofwi <fullofwise@gmail.com>
2021-11-11 22:59:49 +0900

    modified data type of sex: char(max_len=1) -> boolean(male:true, female: false)

--- backend/vesta/kitchen/models.py
+++ backend/vesta/kitchen/models.py
@@ -19,7 +19,7 @@ class Profile(models.Model):
         null=True,                      # should be revised to => 'null=False'
         on_delete=models.CASCADE
     )
-    age = IntegerField(null=True)
+    age = BooleanField(null=True)
     sex = CharField(max_length=1, null=True) # 'M' for Male, 'F' for Female
     height = IntegerField(null=True)
     weight = IntegerField(null=True)
