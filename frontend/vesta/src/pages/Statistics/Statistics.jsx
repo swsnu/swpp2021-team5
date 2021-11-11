882b343b00507c58f0dd59101613bac6d4ec8c99
fuofwi <fullofwise@gmail.com>
2021-11-12 00:49:57 +0900

    code refactorization & write some test code

--- frontend/vesta/src/pages/Statistics/Statistics.jsx
+++ frontend/vesta/src/pages/Statistics/Statistics.jsx
@@ -6,11 +6,10 @@ import { connect } from 'react-redux';
 import {
   Header, Table, Input, Button, Grid
 } from 'semantic-ui-react';
-import { Bar } from 'react-chartjs-2';
-import ChartDataLabels from 'chartjs-plugin-datalabels';
 import styled from 'styled-components';
 
 import * as actionCreators from '../../store/actions/index';
+import StatsDaily from '../../component/Statistics/StatsDaily';
 
 const StatisticsHeader = styled.div`
 font-family:'verveine';
@@ -37,82 +36,22 @@ class Statistics extends Component {
   }
 
   render() {
-    const data = {
-      labels: [
-        'Calorie',
-        'Carbs',
-        'Protein',
-        'Fat',
-      ],
-      datasets: [{
-        // label: "Today's My Nutrient Intake",
-        data: [72, 64, 57, 80],
-        backgroundColor: [
-          'green',
-          'rgb(255, 99, 132)',
-          'rgb(54, 162, 235)',
-          'rgb(255, 205, 86)',
-        ],
-      },
-      ],
-    };
-
-    const options = {
-      scales: {
-        y: {
-          display: true,
-          min: 0,
-          max: 100,
-          ticks: {
-            callback: function (value) {
-              return value + '%'; // convert it to percentage
-            },
-            scaleLabel: {
-              display: true,
-              labelString: 'Percentage',
-            },
-            scaleFontSize: 100
-          },
-        },
-        x: {
-          ticks: {
-            fontSzie: 20
-          }
-        }
-      },
-      plugins: {
-        datalabels: {
-          display: true,
-          color: "white"
-        },
-        legend: {
-          display: false,
-          labels: {
-            font: {
-              size: 1
-            }
-          }
-        },
-      },
-      maintainAspectRatio: true,
-    };
-
     return (
       <div className="Statistics">
 
         
         <div className="Header">
-          <StatisticsHeader>Your Today's intake</StatisticsHeader>
+          <StatisticsHeader>Your Nutritional Statistics</StatisticsHeader>
         </div>
 
         <Grid column={1} textAlign="center">
           <Grid.Column width={5}>
-            <Bar data={data} redraw plugins={[ChartDataLabels]} options={options} width={3} height={3} />
+            <StatsDaily data={[72, 64, 57, 80]} />
             <br/>
             <br/>
-            <Button size='massive'>Today</Button>
-            <Button size='massive'>Weekly</Button>
-            <Button size='massive'>Monthly</Button>
+            <Button size='large'>Today</Button>
+            <Button size='large'>Weekly</Button>
+            <Button size='large'>Monthly</Button>
           </Grid.Column>
         </Grid>
       </div>
