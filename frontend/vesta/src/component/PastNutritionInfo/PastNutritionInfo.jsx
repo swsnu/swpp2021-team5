1d5fe7da124a407c71597d85ca2ce3ddc7c7c094
김남기 <kimnamgi@Kimnamgi-MacBookAir.local>
2021-11-11 17:11:01 +0900

    main page css edited

--- frontend/vesta/src/component/PastNutritionInfo/PastNutritionInfo.jsx
+++ frontend/vesta/src/component/PastNutritionInfo/PastNutritionInfo.jsx
@@ -4,6 +4,17 @@ import { Bar } from 'react-chartjs-2';
 import {
   Container,
 } from 'semantic-ui-react';
+import styled from 'styled-components';
+
+const StatsBody = styled.div`
+background-color:#CCEECC;
+border-radius: 20px;
+font-family:'verveine';
+font-size:30px;
+margin: 15px;
+padding: 10px;
+`;
+
 
 class PastNutritionInfo extends Component {
   constructor(props) {
@@ -20,7 +31,7 @@ class PastNutritionInfo extends Component {
         'Fat',
       ],
       datasets: [{
-        label: 'Nutrition Intake Percentage for Today',
+        label: 'Nutrition Intake Percentage\nfor Today',
         data: [60, 80, 70, 90],
         backgroundColor: [
           'green',
@@ -39,7 +50,7 @@ class PastNutritionInfo extends Component {
         'Fat',
       ],
       datasets: [{
-        label: 'Nutrition Intake Percentage for This Week',
+        label: 'Nutrition Intake Percentage\nfor This Week',
         data: [30, 40, 45, 35],
         backgroundColor: [
           'green',
@@ -49,45 +60,66 @@ class PastNutritionInfo extends Component {
         ],
       }],
     };
+    const options = {
+      scales: {
+        y: {
+          display: true,
+          min: 0,
+          max: 100,
+          ticks: {
+            callback: function (value) {
+              return value + '%'; // convert it to percentage
+            },
+            scaleLabel: {
+              display: true,
+              labelString: 'Percentage',
+            },
+            scaleFontSize: 100
+          },
+        },
+        x: {
+          ticks: {
+            fontSzie: 20
+          }
+        }
+      },
+      plugins: {
+        datalabels: {
+          display: true,
+          color: "white"
+        },
+        legend: {
+          display: false,
+          labels: {
+            font: {
+              size: 1
+            }
+          }
+        },
+      },
+      maintainAspectRatio: true,
+    };
 
     return (
       <div>
-        <Container>
+        <StatsBody>
           <h2>Nutrition Analysis For Today</h2>
           <Bar
             data={dailyData}
             width={30}
             height={20}
-            options={{
-              scales: {
-                y: {
-                  max: 100,
-                  min: 0,
-                  ticks: {
-                    stepSize: 5,
-                  },
-                },
-              },
-            }}
+            options={options}
           />
+        </StatsBody>
+        <StatsBody style={{backgroundColor: "#CCEEFF"}}>
           <h2>Nutrition Analysis For This Week</h2>
           <Bar
             data={weeklyData}
             width={30}
             height={20}
-            options={{
-              scales: {
-                y: {
-                  max: 100,
-                  min: 0,
-                  ticks: {
-                    stepSize: 5,
-                  },
-                },
-              },
-            }}
+            options={options}
           />
-        </Container>
+        </StatsBody>
       </div>
     );
   }
