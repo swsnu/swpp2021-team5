669a360e2ce90bdac97a8dcaf5b888450fae0c4f
김남기 <kimnamgi@Kimnamgi-MacBookAir.local>
2021-11-25 17:44:50 +0900

    crash modified

--- frontend/vesta/src/component/Statistics/GraphConfig.test.js
+++ frontend/vesta/src/component/Statistics/GraphConfig.test.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-import { options } from './GraphConfig';
-
-describe('GraphConfig', () => {
-  it('should return correct value', () => {
-    const result = options.scales.y.ticks.callback(100);
-=======
 import { todayOptions } from './GraphConfig';
 
 describe('GraphConfig', () => {
   it('should return correct value', () => {
     const result = todayOptions.scales.y.ticks.callback(100);
->>>>>>> 83e1fe3a74ff9be70c1cec7f568748db8c401dad
     expect(result).toBe('100%');
   });
 });
