103c76dda117daf61a6c700da9e36a4229d48013
Chris Hong <tippy33@naver.com>
2021-11-25 22:35:08 +0900

    fix calculator

--- frontend/vesta/src/pages/Setting/Calculator.js
+++ frontend/vesta/src/pages/Setting/Calculator.js
@@ -1,8 +1,8 @@
 export const recommendedCalorie = (age, sex, height, weight) => {
-  if (sex === 'M') {
+  if (sex === true) {
     return 66.47 + 13.75 * weight + 5 * height - 6.76 * age;
   }
-  if (sex === 'F') {
+  if (sex === false) {
     return 655.1 + 9.56 * weight + 1.85 * height - 4.68 * age;
   }
   return undefined; // value of sex is invalid
