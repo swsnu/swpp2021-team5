81a1308a3ac6938da877c1b00fef7e58cc155c9d
Chris Hong <tippy33@naver.com>
2021-11-29 04:30:06 +0900

    change recipe component

--- frontend/vesta/src/component/Recipe/Recipe.jsx
+++ frontend/vesta/src/component/Recipe/Recipe.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable */
+import { Component } from 'react';
 import styled from 'styled-components';
 import { Container } from 'semantic-ui-react';
 
@@ -14,11 +15,24 @@ font-size:15px;
 text-align:left;
 `;
 
-const Recipe = (props) => (
-  <Container text className="Recipe">
-    <RecipeHeader>Recipe</RecipeHeader>
-    <TextArea>{props.recipe}</TextArea>
-  </Container>
-);
+class Recipe extends Component {
+  render() {
+    let recipe = '';
+    let regex = new RegExp("'(.*?)'", "g");
+    let match = regex.exec(this.props.recipe);
+    while (match != null) {
+      recipe += match[1]+'\n';
+      console.log(match[1]);
+      match = regex.exec(this.props.recipe);
+    }
+    console.log(recipe);
+    return(
+      <Container text className="Recipe">
+        <RecipeHeader>Recipe</RecipeHeader>
+        <TextArea>{recipe}</TextArea>
+      </Container>
+    );
+  }
+}
 
 export default Recipe;
