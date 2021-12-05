0d0f268991741e16b63578f9dbad582063a7909d
Chris Hong <tippy33@naver.com>
2021-12-05 23:12:57 +0900

    handle null meals, at MealList

--- frontend/vesta/src/component/Meal/MealList.jsx
+++ frontend/vesta/src/component/Meal/MealList.jsx
@@ -67,141 +67,17 @@ class MealList extends Component {
   };
 
   render() {
-    let otherBreakfast = null;
-    let otherLunch = null;
-    let otherDinner = null;
-    if(this.props.recommendedMenus) {
-      otherBreakfast = (
-        <div class="container_other_breakfast">
-          <div class="item_other_breakfast">
-            <a onClick={() => this.onMealDetail(this.props.recommendedMenus[3].name, 3)}>
-              <img
-                id="otherBreakfast"
-                src={this.props.recommendedMenus[3].image}
-                alt="meal_one"
-              />
-            </a>
-            <OtherMealName>{this.props.recommendedMenus[3].name}</OtherMealName>
-          </div>
-          <div class="item_other_breakfast">
-            <a onClick={() => this.onMealDetail(this.props.recommendedMenus[4].name, 4)}>
-              <img
-                src={this.props.recommendedMenus[4].image}
-                alt="meal_one"
-              />
-            </a>
-            <OtherMealName>{this.props.recommendedMenus[4].name}</OtherMealName>
-          </div>
-          <div class="item_other_breakfast">
-            <a onClick={() => this.onMealDetail(this.props.recommendedMenus[5], 5)}>
-              <img
-                src={this.props.recommendedMenus[5].image}
-                alt="meal_one"
-              />
-            </a>
-            <OtherMealName>{this.props.recommendedMenus[5].name}</OtherMealName>
-          </div>
-          <div class="item_other_breakfast">
-            <a onClick={() => this.onMealDetail(this.props.recommendedMenus[6].name, 6)}>
-              <img
-                src={this.props.recommendedMenus[6].image}
-                alt="meal_one"
-              />
-            </a>
-            <OtherMealName>{this.props.recommendedMenus[6].name}</OtherMealName>
-          </div>
-        </div>
-      );
-      otherLunch = (
-        <div class="container_other_lunch">
-        <div class="item_other_lunch">
-          <a onClick={() => this.onMealDetail(this.props.recommendedMenus[7].name, 7)}>
-            <img
-              src={this.props.recommendedMenus[7].image}
-              alt="meal_one"
-            />
-          </a>
-          <OtherMealName>{this.props.recommendedMenus[7].name}</OtherMealName>
-          </div>
-          <div class="item_other_lunch">
-          <a onClick={() => this.onMealDetail(this.props.recommendedMenus[8].name, 8)}>
-            <img
-              src={this.props.recommendedMenus[8].image}
-              alt="meal_one"
-            />
-          </a>
-          <OtherMealName>{this.props.recommendedMenus[8].name}</OtherMealName>
-          </div>
-          <div class="item_other_lunch">
-          <a onClick={() => this.onMealDetail(this.props.recommendedMenus[9].name, 9)}>
-            <img
-              src={this.props.recommendedMenus[9].image}
-              alt="meal_one"
-            />
-          </a>
-          <OtherMealName>{this.props.recommendedMenus[9].name}</OtherMealName>
-          </div>
-          <div class="item_other_lunch">
-          <a onClick={() => this.onMealDetail(this.props.recommendedMenus[10].name, 10)}>
-            <img
-              src={this.props.recommendedMenus[10].image}
-              alt="meal_one"
-            />
-          </a>
-          <OtherMealName>{this.props.recommendedMenus[10].name}</OtherMealName>
-          </div>
-        </div>
-      );
-      otherDinner = (
-        <div class="container_other_dinner">
-          <div class="item_other_dinner">
-          <a onClick={() => this.onMealDetail(this.props.recommendedMenus[11].name, 11)}>
-            <img
-              src={this.props.recommendedMenus[11].image}
-              alt="meal_one"
-            />
-          </a>
-          <OtherMealName>{this.props.recommendedMenus[11].name}</OtherMealName>
-          </div>
-          <div class="item_other_dinner">
-          <a onClick={() => this.onMealDetail(this.props.recommendedMenus[12].name, 12)}>
-            <img
-              src={this.props.recommendedMenus[12].image}
-              alt="meal_one"
-            />
-          </a>
-          <OtherMealName>{this.props.recommendedMenus[12].name}</OtherMealName>
-          </div>
-          <div class="item_other_dinner">
-          <a onClick={() => this.onMealDetail(this.props.recommendedMenus[13].name, 13)}>
-            <img
-              src={this.props.recommendedMenus[13].image}
-              alt="meal_one"
-            />
-          </a>
-          <OtherMealName>{this.props.recommendedMenus[13].name}</OtherMealName>
-          </div>
-          <div class="item_other_dinner">
-          <a onClick={() => this.onMealDetail(this.props.recommendedMenus[14].name, 14)}>
-            <img
-              src={this.props.recommendedMenus[14].image}
-              alt="meal_one"
-            />
-          </a>
-          <OtherMealName>{this.props.recommendedMenus[14].name}</OtherMealName>
-          </div>
-        </div>
-      );
-    }
+    let otherBreakfast = [];
+    let otherLunch = [];
+    let otherDinner = [];
+    let meals = [];
     let recommendationBody = "Here, we recommend you the best meal for today according to your nutriture data for today.\n By clicking Plus icon, you can see other recommended menus, as well. Enjoy!";
     let breakfast_ = "Breakfast  ";
     let lunch_ = "Lunch  ";
     let dinner_ = "Dinner  ";
-    return (
-      <div className="MenuRecommendation">
-        <RecommendationHeader>{this.props.title}</RecommendationHeader>
-        <RecommendationBody>{recommendationBody}</RecommendationBody>
-        <div class="container">
+    if (this.props.recommendedMenus) {
+      if (this.props.recommendedMenus[0] !== null) {
+        meals.push(
           <div class="item">
             <Breakfast>
               <a onClick={() => this.onMealDetail(this.props.recommendedMenus[0].name, 0)}>
@@ -217,6 +93,10 @@ class MealList extends Component {
               </MealName>
             </Breakfast>
           </div>
+        );
+      }
+      if (this.props.recommendedMenus[1] !== null) {
+        meals.push(
           <div class="item">
             <Lunch>
               <a onClick={() => this.onMealDetail(this.props.recommendedMenus[1].name, 1)}>
@@ -232,6 +112,10 @@ class MealList extends Component {
               </MealName>
             </Lunch>
           </div>
+        );
+      }
+      if (this.props.recommendedMenus[2] !== null) {
+        meals.push(
           <div class="item">
             <Dinner>
               <a onClick={() => this.onMealDetail(this.props.recommendedMenus[2].name, 2)}>
@@ -247,10 +131,65 @@ class MealList extends Component {
               </MealName>
             </Dinner>
           </div>
+        );
+      }
+      for (let idx=3; idx<7; idx++) {  // other breakfast
+        if (this.props.recommendedMenus[idx] !== null) {
+          otherBreakfast.push(
+            <div class="item_other_breakfast">
+              <a onClick={() => this.onMealDetail(this.props.recommendedMenus[idx].name, idx)}>
+                <img
+                  id="otherBreakfast"
+                  src={this.props.recommendedMenus[idx].image}
+                  alt="meal_one"
+                />
+              </a>
+              <OtherMealName>{this.props.recommendedMenus[idx].name}</OtherMealName>
+            </div>
+          )     
+        }
+      }
+      for (let idx=7; idx<11; idx++) {  // other lunch
+        if (this.props.recommendedMenus[idx] !== null){
+          otherLunch.push(
+            <div class="item_other_lunch">
+              <a onClick={() => this.onMealDetail(this.props.recommendedMenus[idx].name, idx)}>
+                <img
+                  src={this.props.recommendedMenus[idx].image}
+                  alt="meal_one"
+                />
+              </a>
+              <OtherMealName>{this.props.recommendedMenus[idx].name}</OtherMealName>
+            </div>
+          )
+        }
+      }
+      for (let idx=11; idx<15; idx++) {  // other dinner
+        if (this.props.recommendedMenus[idx] !==  null) {
+          otherDinner.push(
+            <div class="item_other_dinner">
+              <a onClick={() => this.onMealDetail(this.props.recommendedMenus[idx].name, idx)}>
+                <img
+                  src={this.props.recommendedMenus[idx].image}
+                  alt="meal_one"
+                />
+              </a>
+              <OtherMealName>{this.props.recommendedMenus[idx].name}</OtherMealName>
+            </div>
+          )
+        }
+      }
+    }
+    return (
+      <div className="MenuRecommendation">
+        <RecommendationHeader>{this.props.title}</RecommendationHeader>
+        <RecommendationBody>{recommendationBody}</RecommendationBody>
+        <div class="container">
+          {meals}
         </div>
-        {this.state.otherBreakfast ? otherBreakfast : null}
-        {this.state.otherLunch ? otherLunch : null}
-        {this.state.otherDinner ? otherDinner : null}
+        {this.state.otherBreakfast ? <div class="container_other_breakfast"> {otherBreakfast} </div>: null}
+        {this.state.otherLunch ? <div class="container_other_lunch"> {otherLunch} </div>: null}
+        {this.state.otherDinner ? <div class="container_other_dinner"> {otherDinner} </div>: null}
         <Button className="main-button" onClick={() => this.props.history.push('/main')}>Back</Button>
       </div>
     );
