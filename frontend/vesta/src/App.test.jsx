d431ad46bcf8745aba556146b92e1fd8b79da810
Chris Hong <tippy33@naver.com>
2021-12-09 03:27:49 +0900

    add push /login when unauthorized at Main page

--- frontend/vesta/src/App.test.jsx
+++ frontend/vesta/src/App.test.jsx
@@ -1,45 +1,51 @@
-// // import { render, screen } from '@testing-library/react';
-// import { Provider } from 'react-redux';
-// import { mount } from 'enzyme';
-// import App from './App';
-// import { history } from './store/store';
-// import { getMockStore } from './test-utils/mock';
+// import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { mount } from 'enzyme';
+import App from './App';
+import { history } from './store/store';
+import { getMockStore } from './test-utils/mock';
+import { ConnectedRouter } from 'connected-react-router';
+import { Switch } from 'react-router';
+import { Route, Link } from 'react-router-dom';
+import Signup from './pages/Signup/Signup';
 
-// const userInitialState = {
+const userInitialState = {};
+const recordInitialState = {};
+const menuInitialState = {};
+const recipeInitialState = {};
 
-// };
-// const recordInitialState = {
+const mockStore = getMockStore(userInitialState, recordInitialState, menuInitialState, recipeInitialState);
 
-// };
-// const menuInitialState = {
+jest.mock('./pages/Signup/Signup', () => jest.fn(() => <div />));
+jest.mock('./pages/Menu_Recommendation/MenuRecommendation', () => jest.fn(() => <div />));
+jest.mock('./pages/Nutritional_Info_and_Recipe/NutritionalInfoAndRecipe', () => jest.fn(() => <div />));
+jest.mock('./pages/PastMealRecord/PastMealRecord', () => jest.fn(() => <div />));
+jest.mock('./pages/PreviousMeal/PreviousMeal', () => jest.fn(() => <div />));
+jest.mock('./pages/Food_Record/FoodRecord', () => jest.fn(() => <div />));
+jest.mock('./pages/Confirm_Detection/ConfirmDetection', () => jest.fn(() => <div />));
+jest.mock('./pages/Main/Main', () => jest.fn(() => <div />));
+jest.mock('./pages/Setting/Setting', () => jest.fn(() => <div />));
+jest.mock('./pages/Statistics/Statistics', () => jest.fn(() => <div />));
 
-// };
-// const recipeInitialState = {
+describe('App', () => {
+  let app;
+  beforeEach(() => {
+    app = (
+      <Provider store={mockStore}>
+        <App history={history} />
+      </Provider>
+    );
+  });
 
-// };
+  it('should render correctly', () => {
+    const component = mount(app);
+    expect(component.find('.App').length).toBe(1);
+  });
 
-// const mockStore = getMockStore(userInitialState, recordInitialState, menuInitialState, recipeInitialState);
+});
 
-// describe('App', () => {
-//   let app;
-//   beforeEach(() => {
-//     app = (
-//       <Provider store={mockStore}>
-//         <App history={history} />
-//       </Provider>
-//     );
-//   });
-
-//   it('should render correctly', () => {
-//     const component = mount(app);
-//     expect(component.find('.App').length).toBe(1);
-//   });
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
 // });
-
-// // test('renders learn react link', () => {
-// //   render(<App />);
-// //   const linkElement = screen.getByText(/learn react/i);
-// //   expect(linkElement).toBeInTheDocument();
-// // });
-
-describe('.', () => { it('.', () => {}); });
